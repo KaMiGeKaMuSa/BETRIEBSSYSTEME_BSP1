37059c21dd48a628bc4200a75ab813bcc2d2a070
gertsch7003 <gerhard@Gerhards-MacBook-Pro.local>
2016-02-13 11:15:51 +0100

    Changes for Develop Environment
    (gerhard)

---
+++ BETRIEBSSYSTEME_BSP1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BETRIEBSSYSTEME_BSP1
+//
+//  Copyright © 2016 KaMiGeKaMuSa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

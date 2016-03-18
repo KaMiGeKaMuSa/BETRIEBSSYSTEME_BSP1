cecd9a626012dca2c5ed9d27d48fe7d5b6f86cca
gertsch7003 <gerhardsabeditsch@gmx.net>
2016-03-18 18:11:58 +0100

    doxy_config_adapted

--- OUT_PUT_DOXY/html/dir_279cad603ddb8d1bbd156d30cd2e740a.js
+++
@@ -1,4 +0,0 @@
-var dir_279cad603ddb8d1bbd156d30cd2e740a =
-[
-    [ "main.c", "main_8c.html", "main_8c" ]
-];
--- OUT_PUT_DOXY/html/files.js
+++
@@ -1,4 +0,0 @@
-var files =
-[
-    [ "BETRIEBSSYSTEME_BSP1", "dir_279cad603ddb8d1bbd156d30cd2e740a.html", "dir_279cad603ddb8d1bbd156d30cd2e740a" ]
-];
